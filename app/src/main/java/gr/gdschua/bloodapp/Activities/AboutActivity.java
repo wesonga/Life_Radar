a8d166678a9d4e9184004cc5acec62a5dda31607
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2023-08-03 12:27:36 +0300

    fyp

---
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/AboutActivity.java
@@ -0,0 +1,36 @@
+package gr.gdschua.bloodapp.Activities;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import gr.gdschua.bloodapp.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+//
+        TextView licence = findViewById(R.id.LicTv);
+
+        licence.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
